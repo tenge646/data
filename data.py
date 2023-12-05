40892b7864c932917627136591cd670124534f6b
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-05 10:24:57 +0300

    done

--- data.py
+++ data.py
@@ -42,12 +42,12 @@ def word_frequency(sentence):
 if __name__ == "__main__":
     expression1 = "([]{})"
     expression2 = "([)]"
-    print(is_balanced(expression1))  # Output: True
-    print(is_balanced(expression2))  # Output: False
+    print(is_balanced(expression1))  
+    print(is_balanced(expression2))  
 
     input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
     result = remove_duplicates(input_sequence)
-    print(result)  # Output: [2, 3, 4, 5, 6, 7]
+    print(result)  
 
     sentence = "testing to see the out put. This is my test."
     result = word_frequency(sentence)
